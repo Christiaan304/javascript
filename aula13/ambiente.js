32e0fdf0a71349f6686927b7ec6d43a1dadbad24
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-10 23:17:22 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var contador = 1
+
+do
+{
+    console.log(`Passo ${contador}`)
+    contador++
+}
+while (contador <= 10)
+
+/*
+var contador = 1
+
+while (contador <= 10)
+{
+    console.log(`passo ${contador}`)
+    contador++
+}
+*/
