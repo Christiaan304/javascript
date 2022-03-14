f7c6e700b8f3b72c4e01c876beb1ebee0abf854d
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-13 22:54:31 -0300

    .

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,19 @@
 let vec = [2, 1, 4, 8, 7, 55, `CHB`]
 
-vec.push(0)
+vec.push(0, `lorem`, 3)
 vec.sort()
 console.log(vec)
 console.log(`O vetor tem ${vec.length} posições`)
 console.log(vec[1])
+
+let pos = vec.indexOf(3)
+
+if ( pos == -1 )
+{
+    console.log(`O valor não foi encontrado`)
+}
+else
+{
+    console.log(`O valor está na posição ${pos}`)
+}
+
