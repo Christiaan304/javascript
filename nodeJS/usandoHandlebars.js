57b84b268a5635f1441f851afe7ec7c68992d696
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-29 02:36:03 -0300

    .

--- nodeJS/usandoHandlebars.js
+++ nodeJS/usandoHandlebars.js
@@ -1,10 +1,22 @@
 const express = require(`express`)
 const handlebars = require(`express-handlebars`)
+const Sequelize = require(`sequelize`)
 const app = express()
 
 //config
     //template engine
-    app.engine(`handlebars`, handlebars({defaultLayout: `main`}))
+    app.engine(`handlebars`, handlebars.engine({defaultLayout: `main`}))
     app.set(`view engine`, `handlebars`)
+
+    // conexão com o banco de dados mysql
+    const sequelize = new Sequelize(`sistemadecadastro`, `root`, `Crystian@25612342`, {
+        host: `localhost`,
+        dialect: `mysql`
+    })
+
+    // rotas
+    app.get(`/cadastro`, (req, res) => {
+        res.render(`formulario`)
+    })
 
 app.listen(8081, () => console.log(`Servidor rodando`))
